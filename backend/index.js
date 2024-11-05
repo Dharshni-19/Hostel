6ec700ac9310cabec8dc1d0e34a7f9e00c52bbf1
Dharshni-19 <158270179+Dharshni-19@users.noreply.github.com>
2024-11-05 15:59:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 
-const cors = require('cors');
+
 const corsOptions ={
     origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
