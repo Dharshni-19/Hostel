0902dd9e31e69d960e39b8bdf3db44b1d359f650
Dharshni-19 <158270179+Dharshni-19@users.noreply.github.com>
2024-11-14 16:26:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,27 +9,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = ['https://hostel-1-uriu.onrender.com', 'https://famous-caramel-6e3443.netlify.app/'];
-
-// Set up CORS configuration
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    // Check if the origin is in the allowed list
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true // Required if you’re using cookies or authorization headers
-}));
-
-// Handle preflight requests for all routes
-app.options('*', cors());
-
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
 
 const authRoutes = require('./routes/auth');
@@ -48,4 +28,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
